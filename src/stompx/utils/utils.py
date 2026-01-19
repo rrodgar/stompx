512c715d84cf6868dc3584187081578c7f63f508
Rafael <rrg.37s@hotmail.com>
2026-01-19 16:23:58 +0100

    network summary

--- src/stompx/utils/utils.py
+++ src/stompx/utils/utils.py
@@ -2,7 +2,43 @@ import pandas as pd
 import numpy as np
 import networkx as nx
 
-def resumen_red(G):
+def network_summary(G:nx.Graph)->dict:
+    """
+    This function provides a summary of commonly used network-level metrics
+
+    Parameters
+    ----------
+    G : networkx.Graph
+        Undirected network.
+
+    Returns
+    -------
+    dict
+        Dictionary containing network summary statistics:
+        
+        * 'N' : int  
+            Number of nodes in the network.
+        * 'avg_k' : float  
+            Average node degree.
+        * 'C' : float  
+            Average clustering coefficient.
+        * 'rho_A' : float  
+            Spectral radius of the adjacency matrix.
+
+    Notes
+    -----
+    - The spectral radius is computed as the largest real part of the
+      eigenvalues of the adjacency matrix.
+
+    Raises
+    ------
+    TypeError
+        If `G` is not a NetworkX Graph instance.
+    """
+
+
+    if not isinstance(G, nx.Graph):
+        raise TypeError("G must be a networkx.Graph instance.")
     degs = [d for _, d in G.degree()]
     avg_k = np.mean(degs)
     C = nx.average_clustering(G)
